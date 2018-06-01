3bc38ac3228181606c44ca38053698ae3fcd43c0
Amelia Backes <abackes19@bsmschool.org>
2018-06-01 17:37:01 -0500

    starting with a average base

--- co2reading.py
+++ co2reading.py
@@ -5,11 +5,17 @@ import RoboPiLib as RPL
 import time
 
 co = 1
-original = RPL.analogRead(co)
-print original
+print RPL.analogRead(co)
+
+while len(average) <= 100:
+    content = RPL.analogRead(co)
+    average.append(content)
+    if len(average) == 100:
+        base = sum(average) / len(average)
+        print base
+
 
 while True:
     content = RPL.analogRead(co)
-    print content
-    if content - original > 3:
+    if content - base > 3:
         print "BEEP BEEP BEEP"
