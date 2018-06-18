fb534ffadf4a77032e0b954d33f95fc934fbf3f9
abackes19loaner <abackes19@bsmschool.org>
2018-06-18 12:32:51 -0400

    add min base change

--- co2reading.py
+++ co2reading.py
@@ -13,6 +13,7 @@ base = 0
 detect = 0
 content = 0
 x = 0
+y = 0
 
 # ^ setup
 
@@ -29,6 +30,7 @@ while len(average) < 1000:
 while True:
     content = RPL.analogRead(co)
     if content - base >= 2: # a difference >= 2 signifies a significant change
+        y = 0
         print content
         x = x + 1
         detect = 2
@@ -37,18 +39,22 @@ while True:
             print "!!!!"
             detect = 3
             PTW.state['detect'] = 3
-    elif content - base <= -2:
-        average = [ ]
-        while len(average) < 1000:
-            content = RPL.analogRead(co)
-            average.append(content)
-            if len(average) == 1000:
-                base = sum(average) / len(average)
-                print "NEW BASE"
-                print base
+    elif content - base <= -3:
+        x = 0
+        y = y + 1
+        if y >= 3:
+            average = [ ]
+            while len(average) < 1000:
+                content = RPL.analogRead(co)
+                average.append(content)
+                if len(average) == 1000:
+                    base = sum(average) / len(average)
+                    print "NEW BASE"
+                    print base
     else:
         detect = 1
         PTW.state['detect'] = 1
         x = 0
+        y = 0
 
     PTW.post()
