97dcde1b985a11fbfac04b7b125c31b34bd43f6f
abackes19loaner <abackes19@bsmschool.org>
2018-06-18 14:36:22 -0400

    now this actually works, calibrated sensor and adjusted for this

--- co2reading.py
+++ co2reading.py
@@ -13,48 +13,25 @@ base = 0
 detect = 0
 content = 0
 x = 0
-y = 0
 
 # ^ setup
 
 # begins by averaging the first 1000 readings in order to get a base reading
-
-while len(average) < 1000:
-    content = RPL.analogRead(co)
-    average.append(content)
-    if len(average) == 1000:
-        base = sum(average) / len(average)
-        print base
-
+base = RPL.analogRead(co)
 
 while True:
     content = RPL.analogRead(co)
-    if content - base >= 2: # a difference >= 2 signifies a significant change
-        y = 0
-        print content
+    if content <= 190 or base - content >= 50:
         x = x + 1
         detect = 2
         PTW.state['detect'] = 2
         if x >= 3: # this indicates breathing
-            print "!!!!"
             detect = 3
             PTW.state['detect'] = 3
-    elif content - base <= -3:
-        x = 0
-        y = y + 1
-        if y >= 3:
-            average = [ ]
-            while len(average) < 1000:
-                content = RPL.analogRead(co)
-                average.append(content)
-                if len(average) == 1000:
-                    base = sum(average) / len(average)
-                    print "NEW BASE"
-                    print base
+
     else:
         detect = 1
         PTW.state['detect'] = 1
         x = 0
-        y = 0
 
     PTW.post()
