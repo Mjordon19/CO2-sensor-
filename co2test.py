d43a74f8f2c4250ae9c003619948b3fa4ed93c47
abackes19loaner <abackes19@bsmschool.org>
2018-06-25 19:47:41 -0400

    see what numbers the sensor is getting

--- co2test.py
+++ co2test.py
@@ -3,3 +3,16 @@ import RoboPiLib as RPL
 
 while True:
   print RPL.analogRead(1)
+
+#    elif content - base <= -3:
+#        x = 0
+#        y = y + 1
+#        if y >= 3:
+#            average = [ ]
+#            while len(average) < 1000:
+#                content = RPL.analogRead(co)
+#                average.append(content)
+#                if len(average) == 1000:
+#                    base = sum(average) / len(average)
+#                    print "NEW BASE"
+#                    print base
